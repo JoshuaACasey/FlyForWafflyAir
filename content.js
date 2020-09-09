09cd4a0e1d0d0afcbed38d80c5b25fe8c60151e8
Joshua <1041261+JoshuaACasey@users.noreply.github.com>
2020-09-09 08:37:17 -0500

    Update content.js

--- content.js
+++ content.js
@@ -4,10 +4,10 @@
 */
 
 chrome.storage.sync.get(['FlyForWaffleAir'], function(result) {
-	var findFlyForGroup = $("a[href^='javascript:doSubmit4(72560)']").eq(0);
+	var findFlyForGroup = $("a[href^='javascript:doSubmit4(79153)']").eq(0);
 	if (findFlyForGroup && (result.FlyForWaffleAir==="false"||result.FlyForWaffleAir===undefined)) {
 		var form = $('form[name="groupForm"]').eq(0);
-		form.find('input[name="id"]').val(72560);
+		form.find('input[name="id"]').val(79153);
 		form.submit();
 	}
 });
